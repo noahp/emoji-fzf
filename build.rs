1bb778d12bc93e2c01d545728895b5793fbc1d80
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2018-09-05 16:08:01 -0400

    Whoa, sweet.

--- build.rs
+++ build.rs
@@ -2,11 +2,21 @@
 //!
 
 extern crate phf_codegen;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
 
 use std::env;
 use std::fs::File;
 use std::io::{BufWriter, Write};
 use std::path::Path;
+use std::collections::HashMap;
+
+#[derive(Deserialize, Debug)]
+struct EmojiEntry {
+    keywords: Vec<String>,
+    char: String,
+}
 
 fn main() {
     let path = Path::new(&env::var("OUT_DIR").unwrap()).join("codegen.rs");
@@ -16,8 +26,31 @@ fn main() {
         &mut file,
         "static EMOJI_MAP: phf::Map<&'static str, &'static str> = "
     ).unwrap();
-    let mut emoji_map = phf_codegen::Map::new();
-    emoji_map.entry("extern", "another");
-    emoji_map.build(&mut file).unwrap();
+    let mut emoji_phf: phf_codegen::Map<String> = phf_codegen::Map::new();
+
+    let emoji_raw = r#"{
+        "grinning": {
+            "keywords": ["face", "smile", "happy", "joy", ":D", "grin"],
+            "char": "😀",
+            "fitzpatrick_scale": false,
+            "category": "people"
+        },
+        "grimacing": {
+            "keywords": ["face", "grimace", "teeth"],
+            "char": "😬",
+            "fitzpatrick_scale": false,
+            "category": "people"
+        }
+    }"#;
+
+    let emoji_map: HashMap<String, EmojiEntry> = serde_json::from_str(emoji_raw).unwrap();
+
+    println!("{:#?}", emoji_map);
+
+    // for (key, value) in emoji_map {
+    //     emoji_map.entry(key, value);
+    // }
+
+    emoji_phf.build(&mut file).unwrap();
     write!(&mut file, ";\n").unwrap();
 }
