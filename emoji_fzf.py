21b2be77a57231d57cd718ac9a49ab0fa4644885
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2019-03-19 14:51:11 -0400

    🏁 speed up
    Improve launch times by eliminating pkg_resources import, and instead
    generating a python dictionary at package build time from the emojis
    JSON file.
    
    Also update pylint in tox to fix random build error.
    
    This makes the tool a lot faster:
    0.02 - 0.03 seconds now vs 0.15 - 0.18 seconds prior.
    
    Bump version number to 0.1.0.

--- emoji_fzf.py
+++ emoji_fzf.py
@@ -5,27 +5,9 @@ Parse emojilib keyword list and provide it as an fzf --preview input.
 Result is a emoji cli fuzzy search utility 🎉!
 """
 from __future__ import print_function
-import json
-import os
 import sys
-
 import click
-import pkg_resources
-
-# The emoji library is installed relative to this package on install
-if pkg_resources.resource_exists(__name__, "emojilib/emojis.json"):
-    EMOJI_LIB_JSON = pkg_resources.resource_string(__name__, "emojilib/emojis.json")
-else:
-    # it's possible we're running from the source dist instead
-    EMOJI_LIB_JSON = open(
-        os.path.join(os.path.abspath(__file__), "emojilib", "emojis.json"), "r"
-    ).read()
-
-# Load and parse the emoji data now
-EMOJI_LIB_JSON = json.loads(EMOJI_LIB_JSON)
-
-# Sanity check
-assert EMOJI_LIB_JSON, "Dang, emojilib/emojis.json is not installed 😢"
+from emoji_fzf_emojilib import EMOJIS
 
 
 @click.group()
@@ -37,7 +19,7 @@ def cli():
 @cli.command()
 def preview():
     """Return an fzf-friendly search list for emoji"""
-    for key, val in EMOJI_LIB_JSON.items():
+    for key, val in EMOJIS.items():
         click.secho(key, bold=True, nl=False)
         click.echo(u" {}".format(u" ".join(val["keywords"])))
 
@@ -54,7 +36,7 @@ def get(name=None):
     if not name:
         sys.exit(-1)
 
-    render = EMOJI_LIB_JSON[name]["char"]
+    render = EMOJIS[name]["char"]
 
     # include newline only if we're not redirected
     if sys.stdout.isatty():
